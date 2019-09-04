bf7d7e4aedbafd87a3c4edc098ba18056ea2399b
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2019-09-04 10:15:23 +0900

    Initial Commit

---
+++ ScreenCover/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScreenCover
+//
+//  Created by Guillermo Ignacio Enriquez Gutierrez on 2019/09/04.
+//  Copyright © 2019 Guillermo Ignacio Enriquez Gutierrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
