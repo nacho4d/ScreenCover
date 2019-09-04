52185d34196b2722073a3cfc6f58398c72cee5b5
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2019-09-04 10:41:32 +0900

    Minimal example that reproduces possible bug in iOS13

--- ScreenCover/ViewController.swift
+++ ScreenCover/ViewController.swift
@@ -2,19 +2,18 @@
 //  ViewController.swift
 //  ScreenCover
 //
-//  Created by Guillermo Ignacio Enriquez Gutierrez on 2019/09/04.
-//  Copyright © 2019 Guillermo Ignacio Enriquez Gutierrez. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let label = UILabel(frame: view.bounds)
+        label.text = "App contents here. \nUser should NOT be able to see this when app is in background"
+        label.numberOfLines = 0
+        label.frame = CGRect(x: 44, y: 100, width: 280, height: 100)
+        view.addSubview(label)
     }
-
-
 }
 
